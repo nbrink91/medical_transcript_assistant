59c6903447cc27adaafc6bfdfdfe50283f6d5d1c
Nick Brink <nick.brink@azblue.com>
2025-05-22 14:38:43 -0700

    generated logs for gpt-4 and gpt-4o-mini

--- main.py
+++ main.py
@@ -89,9 +89,10 @@ def main():
     api_version = os.getenv("AZURE_OPENAI_API_VERSION")
     endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
     api_key = os.getenv("AZURE_OPENAI_API_KEY")
-    model_name = "gpt-35-turbo"
-    model_name = "gpt-4o"
-    model_name = "gpt-4"
+    # model_name = "gpt-35-turbo"
+    # model_name = "gpt-4o"
+    # model_name = "gpt-4"
+    model_name = "gpt-4o-mini"
 
     client = AzureOpenAI(
         api_version=api_version,
